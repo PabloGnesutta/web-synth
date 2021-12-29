00bd12cb1db91e20fe55a21cefa659af491aad22
Pablo Gnesutta <pablo.gnesutta@gmail.com>
2021-12-29 11:22:30 -0300

    debug

--- frontend/src/components/user-interface/Pad.vue
+++ frontend/src/components/user-interface/Pad.vue
@@ -48,7 +48,7 @@ export default {
     onPadTouchEnd(e) {
       for (let i = 0; i < e.changedTouches.length; i++) {
         this.log('--on end ' + e.changedTouches[i].identifier + ' force ' + e.changedTouches[i].force);
-        if (!e.changedTouches[i].force) {
+        if (e.changedTouches[i].force < 0) {
           this.log(
             '----ended ' +
               e.changedTouches[i].identifier +
