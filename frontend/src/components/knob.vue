bfd0c59d7256c7d13a1b7ded56c15d162f7bde1b
Pablo Gnesutta <pablo.gnesutta@gmail.com>
2022-01-06 23:58:07 -0300

    knob color bug fix?

--- frontend/src/components/knob.vue
+++ frontend/src/components/knob.vue
@@ -6,7 +6,7 @@
     @touchstart="onTouchStart"
   >
     <div class="knob-inner" :style="`transform: rotate(${deg}deg); border-color: ${trackColor}`">
-      <div class="knob-handle"></div>
+      <!-- <div class="knob-handle"></div> -->
     </div>
     <div class="mapped-cmd" v-if="appIsMapping">
       {{ mappedCmd }}
@@ -101,7 +101,7 @@ export default {
       this.knobValue = knobValue;
       this.deg = knobValue.map(0, this.maxKnobVal, 0, this.maxTurningDeg);
       const colorAmount = knobValue.map(0, this.maxKnobVal, 255, 10);
-      this.trackColor = "rgba(100, " + ${colorAmount} + ", 200, 1)";
+      this.trackColor = 'rgba(100, ' + colorAmount + ', 200, 1)';
     },
 
     emitWithKnobValue(knobValue) {
